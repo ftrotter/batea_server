b52a60eeafbe72e59a66583c364284c21772ae6c
Fred Trotter <fred.trotter@gmail.com>
2014-12-09 13:39:39 -0600

    more analysis routes

--- app/routes.php
+++ app/routes.php
@@ -11,6 +11,22 @@
 |
 */
 
+Route::get("/pubmed/{pmid}/json", "PubMedController@get_json");
+Route::get("/pubmed/{pmid}", "PubMedController@article");
+Route::get("/pubmed/", "PubMedController@index");
+
+
+Route::get("/wiki/{wikititle}/", "WikiController@article");
+Route::get("/wiki/{wikititle}/json", "WikiController@get_json");
+Route::get("/wiki/{wikititle}/{revision_id}/json", "WikiController@get_json");
+Route::get("/wiki/{wikititle}/{revision_id}", "WikiController@article");
+
+Route::get("/analysis/{wikititle}/graph_json", "AnalysisController@recurse_graph_json");
+Route::get("/analysis/{wikititle}", "AnalysisController@browse");
+
+
+
+
 Route::get('/', function()
 {
 	return View::make('hello');
