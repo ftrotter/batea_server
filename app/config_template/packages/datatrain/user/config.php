11eca7cfc3fbd89e919803df4d110c2e71f71481
Fred Trotter <fred.trotter@gmail.com>
2015-05-13 00:48:58 -0500

    added from Patch

---
+++ app/config_template/packages/datatrain/user/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+    'loginView'              => 'user::login',
+    'forgotView'             => 'user::forgot',
+    'registrationView'       => 'user::register',
+    'activationEmail'        => 'user::email.activation',
+    'forgotEmail'            => 'user::email.forgot',
+    'forgotConfirmationView' => 'user::forgotconfirmation',
+    'emailSenderAddress'     => "webmaster@example.com",
+    'emailSenderName'        => "Webmaster",
+    'emailActivationSubject' => 'Sample Application Activation',
+    'emailPasswordSubject'   => 'Sample Application Password Reset'
+);
+?>
