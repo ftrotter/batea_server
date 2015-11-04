fa39bf79c6d34b1909c949012a028610139c735e
Fred Trotter <fred.trotter@gmail.com>
2015-11-04 15:15:40 +0000

    handle CORS

--- app/filters.php
+++ app/filters.php
@@ -14,6 +14,17 @@
 App::before(function($request)
 {
 	//
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+
+    if (Request::getMethod() == "OPTIONS") {
+        // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+        $headers = [
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'X-Requested-With, Content-Type, X-Auth-Token, Origin, Authorization'
+        ];
+        return Response::make('You are connected to the API', 200, $headers);
+    }
 });
 
 
